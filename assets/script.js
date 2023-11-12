666d16b689cada1693ba0169436228acdffb5b44
DarrenDoan <doandarren95@gmail.com>
2023-11-13 01:34:37 +1100

    for loop and conditional statement wrapped in function

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,13 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 
-for (var x = 0; x < topics.length; x++) {
-    console.log(topics[x]);
+function listTopics() {
+    for (var x = 0; x < topics.length; x++) {
+        console.log(topics[x]);
+    }
 }
-/*
-var topic = "HTML";
 
-if (topic === 'HTML') {
+function selectTopic() {
+    if (topic === 'HTML') {
     console.log("Lets Study HTML!");
 } else if (topic === 'CSS') {
     console.log("Lets Study CSS!");
@@ -17,4 +18,7 @@ if (topic === 'HTML') {
 } else {
     console.log("Please Try Again!");
 }
-*/
+}
+
+
+
