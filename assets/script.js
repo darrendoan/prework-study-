c4fae43ff6357530c72bd832edf8c4479c516cf9
DarrenDoan <doandarren95@gmail.com>
2023-11-12 17:03:28 +1100

    added conditions and if statements

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,13 @@
-var topics = "HTML, CSS, Git, JavaScript";
-console.log(topics);
+var topic = "HTML";
+
+if (topic === 'HTML') {
+    console.log("Lets Study HTML!");
+} else if (topic === 'CSS') {
+    console.log("Lets Study CSS!");
+} else if (topic === 'Git') {
+    console.log("Lets Study Git!");
+}else if (topic === 'JavaScript') {
+    console.log("Lets Study JavaScript!");
+} else {
+    console.log("Please Try Again!");
+}
